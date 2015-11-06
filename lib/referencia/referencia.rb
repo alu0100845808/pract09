47633ce0e34515d728db7a09e5f119632f637529
alejando David Carrillo Padrón <alu0100845808@ull.edu.es>
2015-11-06 08:59:41 +0000

    Def get autores

--- lib/referencia/referencia.rb
+++ lib/referencia/referencia.rb
@@ -1,5 +1,21 @@
 module Referencia
 
-
-
+    class Entrada
+        
+    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
+            
+        def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
+            @autores = autores
+            @titulo = titulo
+            @serie = serie
+            @editorial = editorial
+            @numedicion = numedicion
+            @fecha = fecha
+            @isbn = isbn
+        end
+    
+        def getautores
+          return @autores
+        end
+    end
 end
--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -4,11 +4,15 @@ describe Referencia do
   before :each do
         @libro = Referencia::Entrada.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf", "4 edition", "(July 7, 2013)", ["ISBN-13: 978-1937785499","ISBN-10: 1937785491"])
     end
-=begin  
+
   it 'has a version number' do
     expect(Referencia::VERSION).not_to be nil
   end
-=end
+
   
-  
+
+      it "Existe un método para obtener el listado de autores" do
+          @libro.getautores.should eq(["Dave Thomas", "Andy Hunt", "Chad Fowler"])
+
+    end
 end
