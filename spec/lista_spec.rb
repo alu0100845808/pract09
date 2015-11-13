b9b2919a17d676b9912b58d05a6be2ac04ed7b26
alejando David Carrillo Padrón <alu0100845808@ull.edu.es>
2015-11-13 10:58:59 +0000

    Creando libros

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -4,7 +4,15 @@ describe Referencia do
     before :each do
     @libro = Referencia::Entrada.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf", "4 edition", "(July 7, 2013)", ["ISBN-13: 978-1937785499","ISBN-10: 1937785491"])
     @libro2 = Referencia::Entrada.new(["Scott Chacon"]," Pro Git 2009th Edition.", "Pro"," Apress; 2009 edition"," (August 27, 2009)", ["ISBN-13: 978-1430218333","ISBN-10: 1430218339."])
-    @lista = Referencia::Lista.new(0)
+    @libro3 =Referencia::Entrada.new(["David Flanagan", "Yukihiro Matsumoto"],"The Ruby Programmin Language",nil,"O'Really Media",1,"2008,2,4",["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
+    @libro4 =Referencia::Entrada.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"],"The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends","The Facets of Ruby","Pragmatic Bookshelf",1,"2012,12,25",["ISBN-10: 1934356379","ISBN-13: 978-1934356371"])
+    @libro5 =Referencia::Entrada.new(["Richard E. Silverman"],"Git Pocket Guide",nil,"O'Really Media",1,"2013, 8, 2",["ISBN-10: 1449325866","ISBN-13: 978-1449325862"])
+    @lista= Referencia::Lista.new(0)
+    @lista2=Referencia::Lista.new(@libro)
+    @lista2.addEnd(@libro2)
+    @lista2.addEnd(@libro3)
+    @lista2.addEnd(@libro4)
+    @lista2.addEnd(@libro5)
     end
     
     it 'Se extrae el primer elemento de la lista' do
@@ -38,5 +46,6 @@ describe Referencia do
 		    @lista.delEnd()
 			expect(@lista.takelast).to eq 0
 		end
+		
 
 end
